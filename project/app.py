c124fa40f9ffab3aca440d5afe3cff911e8345c5
Emily <emdesmo@udel.edu>
2026-02-24 11:11:01 -0500

    cleaning up

--- project/app.py
+++ project/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from auth import init_db, create_user
-from login import render_login
-from homepage import render_homepage
+from project.auth import init_db, create_user
+from project.login import render_login
+from project.homepage import render_homepage
 
 st.set_page_config(page_title="Network Manager", layout="wide")
 
